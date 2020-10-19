703d149ad7baa9a2a8b0ba41bf9be36ce3a35291
ilan_bogo <naligob@gmail.com>
2020-10-19 17:40:27 +0300

    there are some chenges only for nir_birnbaom

---
+++ nirbir.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout<<"nir please delet this branch\n make new one under your name and write 'done'\n and please be fast I have alot of shit to do"<< std::endl;
+}
